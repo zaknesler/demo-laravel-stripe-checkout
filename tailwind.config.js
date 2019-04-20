eccf4551fcd4d9e1ebe0e5c3a3cc72d9b3e425a9
Zak Nesler <zaknesler@gmail.com>
2019-04-20 10:13:45 -0400

    Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    extend: {
+      padding: {
+        '11': '2.75rem'
+      },
+      colors: {
+        brand: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#2b6cb0',
+          800: '#2c5282',
+          900: '#2a4365'
+        }
+      }
+    }
+  },
+  variants: {
+    //
+  },
+  plugins: [
+    //
+  ]
+}
